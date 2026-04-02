e29123be3f213e7fcbaeb254cddcda9267e79b9c
Jhuliño A. Oroya Barreto <jaob.4.95@gmail.com>
2026-04-02 17:53:10 -0500

    Feat: add directory

---
+++ clinica-api/src/test/java/com/soft/io/ClinicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
